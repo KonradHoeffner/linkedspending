dceb1a42fab1e97a68758d97baf8645743184d5f
Tobias Poetschke <tobkolleg@yahoo.de>
2014-04-21 09:47:00 +0200

    now moved newModel() to Converter

--- src/main/java/org/aksw/linkedspending/Converter.java
+++ src/main/java/org/aksw/linkedspending/Converter.java
@@ -1,6 +1,11 @@
 package org.aksw.linkedspending;
 
 
+import com.hp.hpl.jena.rdf.model.*;
+import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.RDFS;
+import com.hp.hpl.jena.vocabulary.XSD;
+
 import java.io.File;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -26,4 +31,37 @@ public class Converter {
 		if(file==null) files.put(name,file= new File(Main.folder+"/"+name+".nt"));
 		return file;
 	}
+
+    /**
+     * Creates a new apache jena model
+     * @return the model
+     */
+	static Model newModel()
+	{
+		Model model = ModelFactory.createMemModelMaker().createDefaultModel();
+		model.setNsPrefix("qb", "http://purl.org/linked-data/cube#");
+		model.setNsPrefix("ls", Main.PROPERTIES.getProperty("urlInstance"));
+		model.setNsPrefix("lso", LSO.URI);
+		model.setNsPrefix("sdmx-subject",	"http://purl.org/linked-data/sdmx/2009/subject#");
+		model.setNsPrefix("sdmx-dimension",	"http://purl.org/linked-data/sdmx/2009/dimension#");
+		model.setNsPrefix("sdmx-attribute",	"http://purl.org/linked-data/sdmx/2009/attribute#");
+		model.setNsPrefix("sdmx-measure",	"http://purl.org/linked-data/sdmx/2009/measure#");
+		model.setNsPrefix("rdfs", RDFS.getURI());
+		model.setNsPrefix("rdf", RDF.getURI());
+		model.setNsPrefix("xsd", XSD.getURI());
+		return model;
+	}
+
+    static public class LSO
+    {
+        static final String URI = Main.PROPERTIES.getProperty("urlOntology");
+        static final public Resource CountryComponent = ResourceFactory.createResource(URI + "CountryComponentSpecification");
+        static final public Resource DateComponentSpecification = ResourceFactory.createResource(URI+"DateComponentSpecification");
+        static final public Resource YearComponentSpecification = ResourceFactory.createResource(URI+"YearComponentSpecification");
+        static final public Resource CurrencyComponentSpecification = ResourceFactory.createResource(URI+"CurrencyComponentSpecification");
+
+        static final public Property refDate = ResourceFactory.createProperty(URI+"refDate");
+        static final public Property refYear = ResourceFactory.createProperty(URI+"refYear");
+        static final public Property completeness = ResourceFactory.createProperty(URI+"completeness");
+    }
 }
