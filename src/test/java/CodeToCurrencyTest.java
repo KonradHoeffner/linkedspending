33d1e377085104886279baedaf9290962e8c0de3
Kevin Kubsch <kevinkubsch@googlemail.com>
2014-05-05 17:21:17 +0200

    Added rest support for Scheduler methods.

--- src/test/java/CodeToCurrencyTest.java
+++ src/test/java/CodeToCurrencyTest.java
@@ -1,24 +1,26 @@
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CodeToCurrencyTest
 {
-	@Test public void testCodeToCurrency() 
-	{
-		Map<String,String> codeToCurrency = new HashMap<>();
-		try(Scanner in = new Scanner(this.getClass().getClassLoader().getResourceAsStream("codetocurrency.tsv")))
-		{
-			while(in.hasNextLine())
-			{
-				String line = in.nextLine();
-				if(line.trim().isEmpty()) {continue;} 
-				String[] tokens = line.split("\t");
-				codeToCurrency.put(tokens[0], tokens[1]);
-			}
-		}
-		assertEquals(codeToCurrency.get("JPY"),"http://dbpedia.org/resource/Japanese_yen");
-	}
+    @Test public void testCodeToCurrency()
+    {
+        Map<String,String> codeToCurrency = new HashMap<>();
+        try(Scanner in = new Scanner(this.getClass().getClassLoader().getResourceAsStream("codetocurrency.tsv")))
+        {
+            while(in.hasNextLine())
+            {
+                String line = in.nextLine();
+                if(line.trim().isEmpty()) {continue;}
+                String[] tokens = line.split("\t");
+                codeToCurrency.put(tokens[0], tokens[1]);
+            }
+        }
+        assertEquals(codeToCurrency.get("JPY"),"http://dbpedia.org/resource/Japanese_yen");
+    }
 }
--- src/test/java/JsonDownloaderTest.java
+++ src/test/java/JsonDownloaderTest.java
@@ -1,6 +1,7 @@
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.aksw.linkedspending.JsonDownloader;
+import org.aksw.linkedspending.ResultsReader;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,56 +11,56 @@ import static org.junit.Assert.assertTrue;
 
 public class JsonDownloaderTest
 {
-	@Test public void testGetPartialResults() throws JsonProcessingException, IOException
-	{
-		JsonDownloader.ResultsReader in = new JsonDownloader.ResultsReader("2013");
-		JsonNode node;
-		while((node=in.read())!=null ){}
-		//fail("not yet finished writing this test");
-	}
+    @Test public void testGetPartialResults() throws JsonProcessingException, IOException
+    {
+        ResultsReader in = new ResultsReader("2013");
+        JsonNode node;
+        while((node=in.read())!=null ){}
+        //fail("not yet finished writing this test");
+    }
 
-	@Test public void testGetDatasetNames() throws IOException
-	{
-		Collection<String> names = JsonDownloader.getDatasetNames();
-		assertTrue(names.size()>300);
-		assertTrue(names.contains("berlin_de"));
-	}
+    @Test public void testGetDatasetNames() throws IOException
+    {
+        Collection<String> names = JsonDownloader.getDatasetNames();
+        assertTrue(names.size()>300);
+        assertTrue(names.contains("berlin_de"));
+    }
     //TODO adjust and uncomment after puzzleTogether is fixed - try to avoid messing with datasets
     /**@Test public void testPuzzleTogether() throws FileNotFoundException,IOException
     {
-        File testfile1 = new File("json/parts/testfile1");
-        try (PrintWriter out = new PrintWriter(testfile1))
-        {
-            out.println("\"results\": [");
-            out.print("{\nhallo\n}");
-        }
+    File testfile1 = new File("src/test/resources/testfile1");
+    try (PrintWriter out = new PrintWriter(testfile1))
+    {
+    out.println("\"results\": [");
+    out.print("{\nhallo\n}");
+    }
 
-        File testfile2 = new File("json/parts/testfile2");
-        try (PrintWriter out = new PrintWriter(testfile2))
-        {
-            out.println("\"results\": [");
-            out.print("{\nwelt\n}");
-        }
+    File testfile2 = new File("src/test/resources/testfile2");
+    try (PrintWriter out = new PrintWriter(testfile2))
+    {
+    out.println("\"results\": [");
+    out.print("{\nwelt\n}");
+    }
 
-        new JsonDownloaderProxy().callPuzzleTogether();
+    new JsonDownloaderProxy().callPuzzleTogether();
 
-        String input= "";
-        try(BufferedReader in = new BufferedReader(new FileReader(new File("json/model"))))
-        {
-            while((input += in.readLine())!=null){}
-        }
-        int result = input.compareTo(
-                "\"results\": [\n" +
-                "{\n" +
-                "hallo\n" +
-                "}," +
-                "\"results\": [\n" +
-                "{\n" +
-                "welt\n" +
-                "},");
-        testfile1.delete();
-        testfile2.delete();
-        assertTrue(result == 0);
+    String input= "";
+    try(BufferedReader in = new BufferedReader(new FileReader(new File("json/model"))))
+    {
+    while((input += in.readLine())!=null){}
+    }
+    int result = input.compareTo(
+    "\"results\": [\n" +
+    "{\n" +
+    "hallo\n" +
+    "}," +
+    "\"results\": [\n" +
+    "{\n" +
+    "welt\n" +
+    "},");
+    testfile1.delete();
+    testfile2.delete();
+    assertTrue(result == 0);
 
     }*/
 
