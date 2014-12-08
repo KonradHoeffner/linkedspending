f4e8a34ac66ab5d66d895c4731bf4fa9c5954476
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2014-12-08 09:20:46 +0100

    More detailed exception status in job history.

--- src/main/java/org/aksw/linkedspending/job/Boss.java
+++ src/main/java/org/aksw/linkedspending/job/Boss.java
@@ -1,5 +1,6 @@
 package org.aksw.linkedspending.job;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -16,38 +17,36 @@ public class Boss implements Runnable
 	{
 		// must not throw any exception because ScheduledExecutorService.scheduleAtFixedRate does not schedule any more after exceptions
 		String datasetName = null;
+		Job job = null;
 		try
 		{
-		log.info("Boss started");
-		Map<String, LinkedSpendingDatasetInfo> lsInfos = LinkedSpendingDatasetInfo.all();
-		Map<String, OpenSpendingDatasetInfo> osInfos = OpenSpendingDatasetInfo.getDatasetInfosCached();
-		// first priority: unconverted datasets
-		Set<String> unconverted = osInfos.keySet();
-		// dont choose one that is already being worked on
-		unconverted.removeAll(Job.all());
-		unconverted.removeAll(lsInfos.keySet());
+			log.info("Boss started");
+			Map<String, LinkedSpendingDatasetInfo> lsInfos = LinkedSpendingDatasetInfo.all();
+			Map<String, OpenSpendingDatasetInfo> osInfos = OpenSpendingDatasetInfo.getDatasetInfosCached();
+			// first priority: unconverted datasets
+			Set<String> unconverted = osInfos.keySet();
+			// dont choose one that is already being worked on
+			unconverted.removeAll(Job.all());
+			unconverted.removeAll(lsInfos.keySet());
 
 
-		if(!unconverted.isEmpty())
-		{
-			datasetName = unconverted.iterator().next();
-			log.info("Boss starting unconverted dataset "+datasetName);
-		} else // are there already converted but outdated ones?
-		{
-			Set<String> converted = osInfos.keySet();
-			converted.removeAll(unconverted);
-			Set<String> outdated = converted.stream().filter(s->LinkedSpendingDatasetInfo.isUpToDate(s)).collect(Collectors.toSet());
-
-			if(!outdated.isEmpty())
+			if(!unconverted.isEmpty())
 			{
-				datasetName = outdated.iterator().next();
-				log.info("Boss starting outdated dataset "+datasetName);
+				datasetName = unconverted.iterator().next();
+				log.info("Boss starting unconverted dataset "+datasetName);
+			} else // are there already converted but outdated ones?
+			{
+				Set<String> converted = osInfos.keySet();
+				converted.removeAll(unconverted);
+				Set<String> outdated = converted.stream().filter(s->LinkedSpendingDatasetInfo.isUpToDate(s)).collect(Collectors.toSet());
+
+				if(!outdated.isEmpty())
+				{
+					datasetName = outdated.iterator().next();
+					log.info("Boss starting outdated dataset "+datasetName);
+				}
 			}
-		}
-		if(datasetName!=null)
-		{
-			Job job = null;
-			try
+			if(datasetName!=null)
 			{
 				job = Job.forDatasetOrCreate(datasetName);
 				boolean finished = new DownloadConvertUploadWorker(datasetName, job, true).get();
@@ -58,26 +57,18 @@ public class Boss implements Runnable
 				}
 
 			}
-			catch (Exception e)
-			{
-				if(job!=null)
-				{
-					// TODO allow restarting of failed state
-					job.setState(State.FAILED);
-					job.addHistory(e.getMessage());
-					log.severe("Dataset "+datasetName+": Unexpected exception: "+e.getMessage());
-					e.printStackTrace();
-				}
-			}
-			return;
+			// TODO detect partially uploaded ones, save number of entries?
+			// TODO prevent endless repeats of failed downloads
+			// TODO remove finished jobs
+			log.info("Boss finds nothing to do");
 		}
-
-		// TODO detect partially uploaded ones, save number of entries?
-		// TODO prevent endless repeats of failed downloads
-		// TODO remove finished jobs
-		log.info("Boss finds nothing to do");
-		} catch(Exception e)
+		catch(Exception e)
 		{
+			if(job!=null)
+			{
+				job.setState(State.FAILED);
+				job.addHistory(e.getMessage()+": "+Arrays.toString(e.getStackTrace()));
+			}
 			log.severe("Dataset "+datasetName+": Unexpected exception: "+e.getMessage());
 			e.printStackTrace();
 		}
